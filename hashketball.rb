1be5702d458a6a8723dc5b40dd3db661542568bc
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-08 19:32:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -174,8 +174,6 @@ def player_numbers(team_name)
       team_data.each do |attribute, data|
         if attribute == :players
           data.each do |player, stat|
-            if player == :number
-              jersey_numbers << player[:number]
         binding.pry
             end
           end
