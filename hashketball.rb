0f7da692eb6df9458734f44126f45c9eac41874a
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-08 19:04:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -136,12 +136,12 @@ def num_points_scored(player_name)
   end
 end
 
-def shoe_size(players_name)
+def shoe_size(player_name)
   game_hash.each do |location, team_data|
     team_data[:players].each do |player|
       player.each do |stat, value|
         if player_name == value
-          return player_name[:shoe]
+          return player[:shoe]
         end
       end
     end
